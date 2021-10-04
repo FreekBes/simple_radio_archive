456cd45c8c7dc0034f619f57cf1025b87d579c1f
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2021-10-04 21:12:31 +0200

    added ft. and ft to artist regex

--- tlparser.php
+++ tlparser.php
@@ -76,7 +76,7 @@
 		}
 	</style>
 	<script>
-	var artistsRegex = /\s\&\s|\sand\s|\swith\s|\sx\s|,\s|\svs\s|\svs\.\s|\sversus\s|\smeets\s|\sfeat\s|\sfeat\.\s|\sfeaturing\s|\spres\s|\spres\.\s|\spresents\s+/gm;
+	var artistsRegex = /\s\&\s|\sand\s|\swith\s|\sx\s|,\s|\svs\s|\svs\.\s|\sversus\s|\smeets\s|\sfeat\s|\sfeat\.\s|\sft\.\s|\sft\s|\sfeaturing\s|\spres\s|\spres\.\s|\spresents\s+/gm;
 	var artistsFix = <?php echo json_encode(json_decode(file_get_contents(".htsettings.json"), true)["tlcreator"]["artist_split_fix"]); ?>;
 
 	function closeMySelf() {
